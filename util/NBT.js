c68d52cf8943e7505924bcc511d3645125e41a75
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2026-01-04 16:03:23 -0500

    refactored reading code
    moved much of the reading code into a class to make decoding a list easier

--- util/NBT.js
+++ util/NBT.js
@@ -1,4 +1,4 @@
-const NBT_TAG_TYPE_TO_ID = {
+const NBT_TAG_TYPE = {
     TAG_END: 0x00,
     TAG_BYTE: 0x01,
     TAG_SHORT: 0x02,
@@ -38,8 +38,137 @@ class NBT{
     }
 }
 
+class NBT_READ_TAG_DATA{
+    constructor(){}
+
+    getTagReadFunction(nbtTagType){
+        switch (nbtTagType) {
+            case NBT_TAG_TYPE.TAG_END:
+                return this.readEndTag;
+            case NBT_TAG_TYPE.TAG_BYTE:
+                return this.readByteTag;
+            case NBT_TAG_TYPE.TAG_SHORT:
+                return this.readShortTag;
+            case NBT_TAG_TYPE.TAG_INT:
+                return this.readIntTag;
+            case NBT_TAG_TYPE.TAG_LONG:
+                return this.readLongTag;
+            case NBT_TAG_TYPE.TAG_FLOAT:
+                return this.readFloatTag;
+            case NBT_TAG_TYPE.TAG_DOUBLE:
+                return this.readDoubleTag;
+            case NBT_TAG_TYPE.TAG_BYTE_ARRAY:
+                return this.readByteArrayTag;
+            case NBT_TAG_TYPE.TAG_STRING:
+                return this.readStringTag;
+            case NBT_TAG_TYPE.TAG_LIST:
+                return this.readListTag;
+            case NBT_TAG_TYPE.TAG_COMPOUND:
+                return this.readCompoundTag;
+            case NBT_TAG_TYPE.TAG_INT_ARRAY:
+                return this.readIntArrayTag;
+            case NBT_TAG_TYPE.TAG_LONG_ARRAY:
+                return this.readLongArrayTag;
+            default:
+                console.error("Unknown NBT tag type, so I cannot give a function! `0x"+ nbtTagType.toString(16) +"`")
+                return null;
+        }
+    }
+
+    // all of these return the value and the amount of bytes read
+    readEndTag(byteArray, startOffset){}
+    readByteTag(byteArray, startOffset){}
+    readShortTag(byteArray, startOffset){}
+    readIntTag(byteArray, startOffset){
+        let value = (byteArray[startOffset+0] << 24) | (byteArray[startOffset+1] << 16) | (byteArray[startOffset+2] << 8) | byteArray[startOffset+3]
+        if(value > ((2**32)/2)-1){ value -= 2**32 } // converted from uint32 to int32. make it signed
+
+        return [value, 4]
+    }
+    readLongTag(byteArray, startOffset){
+        let longTagValue = (BigInt(byteArray[startOffset+0]) << 56n)
+                        |  (BigInt(byteArray[startOffset+1]) << 48n)
+                        |  (BigInt(byteArray[startOffset+2]) << 40n)
+                        |  (BigInt(byteArray[startOffset+3]) << 32n)
+                        |  (BigInt(byteArray[startOffset+4]) << 24n)
+                        |  (BigInt(byteArray[startOffset+5]) << 16n)
+                        |  (BigInt(byteArray[startOffset+6]) << 8n)
+                        |  (BigInt(byteArray[startOffset+7]) << 0n)
+        if(longTagValue > ((2**64)/2)-1){ longTagValue -= 2**64 } // converted from uint64 to int64. make it signed
+        return [longTagValue, 8]
+    }
+    readFloatTag(byteArray, startOffset){}
+    readDoubleTag(byteArray, startOffset){}
+    readByteArrayTag(byteArray, startOffset){}
+    readStringTag(byteArray, startOffset){
+        let stringTagLength = (byteArray[startOffset + 0] << 8) | byteArray[startOffset + 1]
+        let stringTagValue = "";
+        for(let stringTagValueIndex=0; stringTagValueIndex<stringTagLength; stringTagValueIndex++){
+            stringTagValue += String.fromCharCode( byteArray[startOffset + 2 + stringTagValueIndex] );
+        }
+
+        // 2 bytes for the length, and then how many we are going to read for the name
+        let bytesRead = 2 + stringTagLength;
+        return [stringTagValue, bytesRead]
+    }
+    readListTag(byteArray, startOffset){
+        let listTagType = byteArray[startOffset + 0];
+        let lengthOfList = (byteArray[startOffset+1] << 24) | (byteArray[startOffset+2] << 16) | (byteArray[startOffset+3] << 8) | byteArray[startOffset+4]
+        
+        let bytesRead = 5;
+        let listValues = []
+        for(let i=0; i<lengthOfList; i++){
+            let [valueOfObj, bytesReadForValue] = (this.getTagReadFunction(listTagType))(byteArray, startOffset+bytesRead);
+            let valueTag = new NBT_TAG(listTagType, i.toString());
+            if(Array.isArray(valueOfObj) == false){ valueOfObj = [valueOfObj] } // put into array if it's not an array
+            valueTag.values = valueOfObj;
+            
+            listValues.push(valueTag);
+            bytesRead += bytesReadForValue;
+        }
+        
+        // 1 byte for the type of tag in the list, and 4 bytes for the length of the list
+        return [listValues, bytesRead]
+    }
+    readCompoundTag(byteArray, startOffset){}
+    readIntArrayTag(byteArray, startOffset){}
+    readLongArrayTag(byteArray, startOffset){
+        let longArrayLength = (byteArray[startOffset+0] << 24) | (byteArray[startOffset+1] << 16) | (byteArray[startOffset+2] << 8) | byteArray[startOffset+3]
+        if(longArrayLength > ((2**32)/2)-1){ longArrayLength -= 2**32 } // converted from uint32 to int32. make it signed
+
+        // 4 bytes for the amount to read (will add to the return also)
+        startOffset += 4
+
+        let valuesArray = [];
+        for(let i=0; i<longArrayLength; i++){
+            let longArrOffset = startOffset + i*8
+            let longTagReadValue = this.readLongTag(byteArray, longArrOffset)[0] // index 0 b/c thats the value
+
+            /*
+            let longTagReadValue = (BigInt(byteArray[longArrOffset+0]) << 56n)
+                                |  (BigInt(byteArray[longArrOffset+1]) << 48n)
+                                |  (BigInt(byteArray[longArrOffset+2]) << 40n)
+                                |  (BigInt(byteArray[longArrOffset+3]) << 32n)
+                                |  (BigInt(byteArray[longArrOffset+4]) << 24n)
+                                |  (BigInt(byteArray[longArrOffset+5]) << 16n)
+                                |  (BigInt(byteArray[longArrOffset+6]) << 8n)
+                                |  (BigInt(byteArray[longArrOffset+7]) << 0n)
+            if(longTagReadValue > ((2**64)/2)-1){ longTagReadValue -= 2**64 } // converted from uint64 to int64. make it signed
+            */
+            valuesArray.push(longTagReadValue);
+        }
+
+        // 4 bytes for the amount to read, amount of bytes read for the list
+        let longArrayBytesRead = 4 + 8 * longArrayLength;
+
+        return [valuesArray, longArrayBytesRead]
+    }
+}
+
 function createNBTFromByteArray(byteArray){
     console.log(byteArray);
+
+    let nbtTagValueReader = new NBT_READ_TAG_DATA();
 
     let stackOfCompounds = []; // stack/heap of compound tags.
     // All tags will be added to the compound at the bottom
@@ -79,152 +208,113 @@ function createNBTFromByteArray(byteArray){
 
         console.log("Reading tag with id: 0x" + tagTypeByte.toString(16));
         switch (tagTypeByte) {
-            case NBT_TAG_TYPE_TO_ID.TAG_END:
+            case NBT_TAG_TYPE.TAG_END:
                 endTag();
                 // end tags cannot have a name so we must remove it
                 amountToAddToIndex -= (2 + tagNameLength)
                 console.log("End tag made, backing out of 1 compound")
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_BYTE:
+            case NBT_TAG_TYPE.TAG_BYTE:
                 //newNBT.addTag_BYTE();
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_SHORT:
+            case NBT_TAG_TYPE.TAG_SHORT:
                 //newNBT.addTag_SHORT();
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_INT:
-                let intTag = new NBT_TAG(NBT_TAG_TYPE_TO_ID.TAG_INT, tagName);
+            case NBT_TAG_TYPE.TAG_INT:
+                let intTag = new NBT_TAG(NBT_TAG_TYPE.TAG_INT, tagName);
 
-                let intTagValue = (byteArray[index+amountToAddToIndex+0] << 24) | (byteArray[index+amountToAddToIndex+1] << 16) | (byteArray[index+amountToAddToIndex+2] << 8) | byteArray[index+amountToAddToIndex+3]
-                if(intTagValue > ((2**32)/2)-1){ intTagValue -= 2**32 } // converted from uint32 to int32. make it signed
-
+                let [intTagValue, intTagBytesRead] = nbtTagValueReader.readIntTag(byteArray, index+amountToAddToIndex);
                 intTag.values = [intTagValue];
 
                 addTag(intTag);
 
                 // 4 bytes for the value
-                amountToAddToIndex += 4;
+                amountToAddToIndex += intTagBytesRead;
                 
                 console.log("Created an Int Tag w/ name: `" + tagName + "` and a value of: `" + intTagValue + "`");
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_LONG:
-                let longTag = new NBT_TAG(NBT_TAG_TYPE_TO_ID.TAG_LONG, tagName);
-
-                let longTagValue = (byteArray[index+amountToAddToIndex+0] << 56)
-                    | (byteArray[index+amountToAddToIndex+1] << 48)
-                    | (byteArray[index+amountToAddToIndex+2] << 40)
-                    | (byteArray[index+amountToAddToIndex+3] << 32)
-                    | (byteArray[index+amountToAddToIndex+4] << 24)
-                    | (byteArray[index+amountToAddToIndex+5] << 16)
-                    | (byteArray[index+amountToAddToIndex+6] << 8)
-                    | (byteArray[index+amountToAddToIndex+7] << 0)
-                if(longTagValue > ((2**64)/2)-1){ longTagValue -= 2**64 } // converted from uint64 to int64. make it signed
-
+            case NBT_TAG_TYPE.TAG_LONG:
+                let longTag = new NBT_TAG(NBT_TAG_TYPE.TAG_LONG, tagName);
+                let [longTagValue, longTagBytesRead] = nbtTagValueReader.readLongTag(byteArray, index+amountToAddToIndex)
                 longTag.values = [longTagValue];
 
-                addTag(longTag);
+                addTag(longTagValue);
 
                 // 8 bytes for the value
-                amountToAddToIndex += 8;
+                amountToAddToIndex += longTagBytesRead;
                 
                 console.log("Created an Long Tag w/ name: `" + tagName + "` and a value of: `" + longTagValue + "`");
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_FLOAT:
+            case NBT_TAG_TYPE.TAG_FLOAT:
                 //newNBT.addTag_FLOAT();
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_DOUBLE:
+            case NBT_TAG_TYPE.TAG_DOUBLE:
                 //newNBT.addTag_DOUBLE();
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_BYTE_ARRAY:
+            case NBT_TAG_TYPE.TAG_BYTE_ARRAY:
                 //newNBT.addTag_BYTE_ARRAY();
 
                 let amountOfElementsByteArr = 0; // todo fill this out
                 
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_STRING:
-                let stringTagLength = (byteArray[index + amountToAddToIndex + 0] << 8) | byteArray[index + amountToAddToIndex + 1]
-                let stringTagValue = "";
-                for(let stringTagValueIndex=0; stringTagValueIndex<stringTagLength; stringTagValueIndex++){
-                    stringTagValue += String.fromCharCode( byteArray[index + amountToAddToIndex + 2 + stringTagValueIndex] );
-                }
-
-                let stringTag = new NBT_TAG(NBT_TAG_TYPE_TO_ID.TAG_STRING, tagName);
+            case NBT_TAG_TYPE.TAG_STRING:
+                let stringTag = new NBT_TAG(NBT_TAG_TYPE.TAG_STRING, tagName);
+                let [stringTagValue, stringTagBytesRead] = nbtTagValueReader.readStringTag(byteArray, index + amountToAddToIndex);
                 stringTag.values = [stringTagValue];
+                console.log(stringTagValue, stringTagBytesRead)
                 
-                addTag(stringTag)
-
-                // 2 bytes for the length, and then how many we are going to read for the name
-                amountToAddToIndex += 2 + stringTagLength;
+                addTag(stringTag);
+                amountToAddToIndex += stringTagBytesRead;
                 
                 console.log("Created String Tag w/ name: `" + tagName + "` and a value of: `" + stringTagValue + "`");
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_LIST:
-                let listTagTypes = byteArray[index + amountToAddToIndex + 0];
-                let lengthOfList = (byteArray[index+amountToAddToIndex+1] << 24) | (byteArray[index+amountToAddToIndex+2] << 16) | (byteArray[index+amountToAddToIndex+3] << 8) | byteArray[index+amountToAddToIndex+4]
+            case NBT_TAG_TYPE.TAG_LIST:
+                let listTagType = byteArray[index + amountToAddToIndex]; // here so i can log it
 
-                let listTag = new NBT_TAG(NBT_TAG_TYPE_TO_ID.TAG_LIST, tagName);
-                
-                // todo: fill values array with tags
+                let listTag = new NBT_TAG(NBT_TAG_TYPE.TAG_LIST, tagName);
+                let [listTagValue, listTagBytesRead] = nbtTagValueReader.readListTag(byteArray, index + amountToAddToIndex);
+                listTag.values = listTagValue
 
                 addTag(listTag)
                 
                 // 1 byte for the type of tag in the list, and 4 bytes for the length of the list
-                amountToAddToIndex += 5;
+                amountToAddToIndex += listTagBytesRead;
 
-                console.log("[MISSING FEATURE] The list tag isn't fully implemented! Come and finish it!!!")
+                //console.log("[MISSING FEATURE] The list tag isn't fully implemented! Come and finish it!!!")
 
-                console.log("Created List Tag w/ name: `" + tagName + "`, a list type of: `" + listTagTypes + "` and a length of `"+lengthOfList+"`");
+                console.log("Created List Tag w/ name: `" + tagName + "`, a list type of: `0x" + listTagType.toString(16) + "`, a length of `"+ listTag.values.length +"`, and values of: ");
+                console.log(listTag.values);
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_COMPOUND:
+            case NBT_TAG_TYPE.TAG_COMPOUND:
                 // compounds are basically just an NBT themselves, and so i will reuse it (and basically the root is always a compound)
                 let newCompoundTag = new NBT(tagName);
                 stackOfCompounds.push(newCompoundTag);
 
                 console.log("Created Compound Tag w/ name: `" + tagName + "`");
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_INT_ARRAY:
+            case NBT_TAG_TYPE.TAG_INT_ARRAY:
                 //newNBT.addTag_INT_ARRAY();
 
                 let amountOfElementsIntArr = 0; // todo fill this out
 
                 break;
-            case NBT_TAG_TYPE_TO_ID.TAG_LONG_ARRAY:
-                let longArrayLength = (byteArray[index+amountToAddToIndex+0] << 24) | (byteArray[index+amountToAddToIndex+1] << 16) | (byteArray[index+amountToAddToIndex+2] << 8) | byteArray[index+amountToAddToIndex+3]
-                if(longArrayLength > ((2**32)/2)-1){ longArrayLength -= 2**32 } // converted from uint32 to int32. make it signed
-
-                // 4 bytes for the amount to read
-                amountToAddToIndex += 4
-
-                let longArrayTag = new NBT_TAG(NBT_TAG_TYPE_TO_ID.TAG_LONG_ARRAY, tagName);
-                for(let i=0; i<longArrayLength; i++){
-                    let longTagReadValue = (byteArray[index+amountToAddToIndex+0 + i*8] << 56)
-                                        |  (byteArray[index+amountToAddToIndex+1 + i*8] << 48)
-                                        |  (byteArray[index+amountToAddToIndex+2 + i*8] << 40)
-                                        |  (byteArray[index+amountToAddToIndex+3 + i*8] << 32)
-                                        |  (byteArray[index+amountToAddToIndex+4 + i*8] << 24)
-                                        |  (byteArray[index+amountToAddToIndex+5 + i*8] << 16)
-                                        |  (byteArray[index+amountToAddToIndex+6 + i*8] << 8)
-                                        |  (byteArray[index+amountToAddToIndex+7 + i*8] << 0)
-                    console.log(longTagReadValue.toString(16))
-                    if(longTagReadValue > ((2**64)/2)-1){ longTagReadValue -= 2**64 } // converted from uint64 to int64. make it signed
-                    longArrayTag.values.push(longTagReadValue);
-                }
-
-                // amount of bytes read for the list
-                amountToAddToIndex += 4 + 8 * longArrayLength;
-
+            case NBT_TAG_TYPE.TAG_LONG_ARRAY:
+                let longArrayTag = new NBT_TAG(NBT_TAG_TYPE.TAG_LONG_ARRAY, tagName);
+                let [longArrayValues, longArrayBytesRead] = nbtTagValueReader.readLongArrayTag(byteArray, index+amountToAddToIndex);
+                longArrayTag.values = longArrayValues
                 addTag(longArrayTag)
 
-                console.log("Created Long Array Tag w/ name: `" + tagName + "`, a length of `"+longArrayLength+"`, and and values of: ");
+                amountToAddToIndex += longArrayBytesRead
+
+                console.log("Created Long Array Tag w/ name: `" + tagName + "`, a length of `"+longArrayValues.length+"`, and and values of: ");
                 console.log(longArrayTag.values)
                 break;
             default:
-                console.error("Unknown tag `"+ tagTypeByte +"`! Either data isn't NBT or there is an error! Index: "+index);
+                console.error("Unknown tag `0x"+ tagTypeByte.toString(16) +"`! Either data isn't NBT or there is an error! Index: "+index);
                 breakFromError = true;
                 break;
         }
 
         index += amountToAddToIndex;
     }
-
-    return newNBT;
 }
